55e378a7d4b4cc19e13a440a1ffce87c21a8a2ec
dmitry-vychikov <diman896@gmail.com>
2021-06-01 01:10:11 +0300

    Reset onboarding when logged out

--- soulmate2/lib/on_boarding/on_boarding_cubit.dart
+++ soulmate2/lib/on_boarding/on_boarding_cubit.dart
@@ -1,10 +1,24 @@
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:soulmate2/auth/firebase/firebase_auth_bloc.dart';
 
 part 'on_boarding_state.dart';
 
 class OnBoardingCubit extends HydratedCubit<OnBoardingState> {
-  OnBoardingCubit() : super(OnBoardingState());
+  final FirebaseAuthBloc _authBloc;
+
+  late final StreamSubscription _authSub;
+  OnBoardingCubit(this._authBloc) : super(OnBoardingState()) {
+    _authSub = _authBloc.stream.listen((state) {
+      if(state is FirebaseAuthInitial){
+        reset();
+      } else if(state is FirebaseAuthCompleted){
+        complete();
+      }
+    });
+  }
 
 
   @override
@@ -13,8 +27,19 @@ class OnBoardingCubit extends HydratedCubit<OnBoardingState> {
     super.onChange(change);
   }
 
+
+  @override
+  Future<void> close() async {
+    await _authSub.cancel();
+    return await super.close();
+  }
+
   void complete() {
     emit(OnBoardingState(isCompleted: true));
+  }
+
+  void reset(){
+    emit(OnBoardingState(isCompleted: false));
   }
 
   @override
